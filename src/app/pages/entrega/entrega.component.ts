d39b08f2ad655214775d8136ba924c6005db0ee9
Mitch-Coder <michellescalderon2002@gmail.com>
2023-05-21 17:51:22 -0600

    Fix table OrdenPedido and Entrega

--- src/app/pages/entrega/entrega.component.ts
+++ src/app/pages/entrega/entrega.component.ts
@@ -1,36 +1,68 @@
 import { Component } from '@angular/core';
 import { TableColumn } from 'src/app/modules/table/models/table-column';
 import { MyDataServices } from 'src/app/services/mydata.services';
-import { tap } from 'rxjs';
+import { forkJoin, map, tap } from 'rxjs';
+
+interface Entrega{
+  codEntrega:number;
+  idEstadoEntrega:number;
+  fechaEntrega:string;
+  descripcion:string;
+}
+interface EstadoEntrega{
+  idEstadoEntrega:number;
+  estadoEntrega:string;
+}
+
 
 @Component({
   selector: 'app-entrega',
   templateUrl: './entrega.component.html',
   styleUrls: ['./entrega.component.css']
 })
+
+
 export class EntregaComponent {
-    
-  myData: Array<object> = [] ;
+  myData: any[] = [];
   myData$:any;
 
   tableColumns: TableColumn[] =[]
-
   constructor(private dataService:MyDataServices){}
-  ngOnInit(): void{
 
-    this.myData$ = this.dataService
-    .getData('entrega')
-    .pipe(tap((data) =>{
-     console.log(data)
-     this.myData = data
-    }))
+ngOnInit(): void{
 
-    this.setTableColumns();
- }
+  this.myData$=forkJoin(
+    this.dataService.getData('entrega'),
+    this.dataService.getData('estadoentrega'),
+  ).pipe(
+    map((data:any[])=>{
+      let entrega:Entrega[] = data[0];
+      let estadoEntrega:EstadoEntrega[] = data[1];
+    
+
+      entrega.forEach( element =>{
+        let state =estadoEntrega.filter(e => e.idEstadoEntrega == element.idEstadoEntrega);
+
+        this.myData.push(
+          {
+            estadoEntrega:state[0].estadoEntrega,
+            codEntrega:element.codEntrega,
+            fechaEntrega:element.fechaEntrega,
+            descripcion:element.descripcion
+          }
+        )
+      })
+      console.log(this.myData)
+      return this.myData
+    })
+  )
+
+  this.setTableColumns();
+}
  
  setTableColumns(){
    this.tableColumns=[
-     {label:'IdEstadoEntrega', def:'idEstadoEntrega', dataKey:'idEstadoEntrega'},
+     {label:'EstadoEntrega', def:'estadoEntrega', dataKey:'estadoEntrega'},
      {label:'Codigo de entrega', def:'codEntrega', dataKey:'codEntrega'},
      {label:'Fecha de entrega', def:'fechaEntrega', dataKey:'fechaEntrega'},
      {label:'Descripcion', def:'descripcion', dataKey:'descripcion'},
--- src/app/pages/orden-pedido/orden-pedido.component.ts
+++ src/app/pages/orden-pedido/orden-pedido.component.ts
@@ -4,24 +4,54 @@ import { MyDataServices } from 'src/app/services/mydata.services';
 import { elementAt, map, mergeMap, tap } from 'rxjs';
 import { forkJoin } from 'rxjs';
 
+//Interfaz Principal
 interface OrdenPedido{
-  idOrdenPedido_Entrega:string;
   numOrden:number;
-  codEntrega:number;
-}
-
-interface Entrega{
-  codEntrega:number;
-  idEstadoEntrega:number;
-  fechaEntrega:string;
+  numExamen:number;
+  numEmpleado:number;
+  idLaboratorio:number;
+  codProducto:number;
+  idEstadoPedido:number;
+  idFechaPedido:number;
+  costo:number;
   descripcion:string;
 }
+//Auxiliares
+
+interface EstadoPedido{
+  idEstadoPedido:number;
+	estadoPedido:string;
+}
+
+interface FechaPedido{
+  idFechaPedido:number;
+	fechaPedido:string;
+}
+
+interface Laboratorio{
+  idLaboratorio:number;
+	nombre:string;
+	direccion:string;
+	telefono:string;
+	correo:string;
+}
+
+interface Empleado{
+  numEmpleado:number;
+	nombres:string;
+	apellidos:string;
+	dirreccion:string;
+}
+
+
 
 @Component({
   selector: 'app-orden-pedido',
   templateUrl: './orden-pedido.component.html',
   styleUrls: ['./orden-pedido.component.css']
 })
+
+
 export class OrdenPedidoComponent {
   myData: any[] = [];
   myData$:any;
@@ -31,28 +61,62 @@ export class OrdenPedidoComponent {
   constructor(private dataService:MyDataServices){}
 
   ngOnInit(): void{
-    this.myData$ = this.dataService
-    .getData('ordenpedido')
-    .pipe(tap((data) =>{
-     console.log(data)
-     this.myData = data
-    }))
+
+    this.myData$ = forkJoin(
+      this.dataService.getData('ordenpedido'),
+      this.dataService.getData('estadopedido'),
+      this.dataService.getData('fechapedido'),
+      this.dataService.getData('laboratorio'),
+      this.dataService.getData('empleado')
+
+    ).pipe(
+      map((data:any[]) => {
+        //Definimos las variables donde se van a guardar cada array de datos consultados
+        let ordenpedido:OrdenPedido[] = data[0];
+        let estadopedido:EstadoPedido[] = data[1];
+        let fechapedido:FechaPedido[] = data[2];
+        let laboratorio:Laboratorio[] = data[3];
+        let empleado:Empleado[] = data[4];
+
+
+        ordenpedido.forEach(element => {
+          let estadopedidoF = estadopedido.filter(e=>e.idEstadoPedido == element.idEstadoPedido)
+          let fechapedidoF = fechapedido.filter(e=>e.idFechaPedido == element.idFechaPedido)
+          let laboratorioF = laboratorio.filter(e=>e.idLaboratorio == element.idLaboratorio)
+          let empleadoF = empleado.filter(e=>e.numEmpleado == element.numEmpleado)
+
+          this.myData.push({
+            //Aqui se tiene que especificar los elementos a mostrar 
+            numOrden:element.numOrden,
+            numExamen:element.numExamen,
+            descripcion:element.descripcion,
+            costo:element.costo,
+            codProducto:element.codProducto,
+            estadopedido:estadopedidoF[0].estadoPedido,
+            fechapedido:fechapedidoF[0].fechaPedido,
+            laboratorio:laboratorioF[0].nombre,
+            empleado:empleadoF[0].nombres,
+          })
+        })
+        console.log(this.myData)
+        return this.myData;
+      })
+    )
 
     this.setTableColumns();
   }
 
   setTableColumns(){
     this.tableColumns=[
-      {label:'CodProducto', def:'codProducto', dataKey:'codProducto'},
-      {label:'Costo', def:'costo', dataKey:'costo'},
-      {label:'Descripcion', def:'descripcion', dataKey:'descripcion'},
-      {label:'IdEstadoPedido', def:'idEstadoPedido', dataKey:'idEstadoPedido'},
-      {label:'IdFechaPedido', def:'idFechaPedido', dataKey:'idFechaPedido'},
-      {label:'IdLaboratorio', def:'idLaboratorio', dataKey:'idLaboratorio'},
-      {label:'N* Empleado', def:'numEmpleado', dataKey:'numEmpleado'},
-      {label:'N* Examen', def:'numExamen', dataKey:'numExamen'},
       {label:'N* Orden', def:'numOrden', dataKey:'numOrden'},
-
+      {label:'N* Examen', def:'numExamen', dataKey:'numExamen'},
+      {label:'Descripcion', def:'descripcion', dataKey:'descripcion'},
+      {label:'Costo', def:'costo', dataKey:'costo'},
+      {label:'CodProducto', def:'codProducto', dataKey:'codProducto'},
+      {label:'EstadoPedido', def:'estadoPedido', dataKey:'estadopedido'},
+      {label:'FechaPedido', def:'fechaPedido', dataKey:'fechapedido'},
+      {label:'Laboratorio', def:'laboratorio', dataKey:'laboratorio'},
+      {label:'N* Empleado', def:'numEmpleado', dataKey:'empleado'},
     ]
   }
 
